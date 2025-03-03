910c30f754b226d688c3e207eb68caf0635a596f
aahenry51 <aahenry51@gmail.com>
2025-03-02 23:17:18 -0500

    Route token

--- auth-flow/src/router/privateRoute.js
+++ auth-flow/src/router/privateRoute.js
@@ -1,6 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
 
-export const PrivateRoutes = () => {
-  let auth = { token: false };
+export const PrivateRoutes = (t) => {
+  const { auth, setAuth } = useContext(AuthContext);
+  //let auth = { token: false };
   return auth.token ? <Outlet /> : <Navigate to="/login" />;
 };
